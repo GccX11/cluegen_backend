2ce380c131a9bf2b82cb8b5e9027c7a3d7fc9a4c
GccX11 <GccX11@users.noreply.github.com>
2023-08-20 14:59:45 -0400

    Updated the cluegen.py to make better clues, mainly because the clue list is better now.

--- cluegen.py
+++ cluegen.py
@@ -1,3 +1,4 @@
+import os
 import spacy
 import numpy as np
 from scipy.cluster.hierarchy import dendrogram, linkage
@@ -6,18 +7,8 @@ from scipy.spatial.distance import pdist
 
 nlp = spacy.load('en_core_web_lg')
 
-# load top_words.txt and get the lemma for them in a map
-# top words from: https://www.ef.edu/english-resources/english-vocabulary/top-3000-words/
-all_clues = []
-with open('projects/cluegen_backend/top_words.txt', 'r') as f:
-    for line in f:
-        token = line.strip()
-        lemma = nlp(token)
-        if lemma.has_vector:
-            all_clues.append(lemma)
 
-
-class Leaf(object):
+class _Leaf(object):
     def __init__(self, word):
         self.lemma = nlp(word)
     
@@ -25,7 +16,7 @@ class Leaf(object):
         return self.lemma.text
 
 
-class Cluster(object):
+class _Cluster(object):
     def __init__(self, distance, children):
         self.distance = distance
         self.children = children
@@ -35,7 +26,7 @@ class Cluster(object):
         # populate the lemmas array if it is empty
         if len(self.lemmas) == 0:
             for child in self.children:
-                if isinstance(child, Leaf):
+                if isinstance(child, _Leaf):
                     self.lemmas.append(child.lemma)
                 else:
                     self.lemmas.extend(child.get_lemmas())
@@ -45,93 +36,100 @@ class Cluster(object):
         return str(self.get_lemmas()) + ', ' + str(np.round(self.distance, 2))
 
 
-# create a proper tree structure,
-# where each node has the average distance of its children
-def convert_to_tree(pairs, words):
-    leaves = {}
-    clusters = {}
+class ClueGenerator(object):
+    def __init__(self, path):
+        # words originally from https://github.com/first20hours/google-10000-english
+        # with additional filtering by me
+        self.all_clue_words = np.load(os.path.join(path, 'all_clue_words.npy'))
+        self.all_clue_vectors = np.load(os.path.join(path, 'all_clue_vectors.npy'))
 
-    for i, row in enumerate(pairs):
-        if row[0] < len(words):
-            # if it is an original point read it from the centers array
-            a = words[int(row[0])]
-            a = Leaf(a)
-            leaves[row[0]] = a
-        else:
-            # other wise read the cluster that has been created
-            a = clusters[int(row[0])]
+    # create a proper tree structure,
+    # where each node has the average distance of its children
+    def convert_to_tree(self, pairs, words):
+        leaves = {}
+        clusters = {}
 
-        if row[1] < len(words):
-            b = words[int(row[1])]
-            b = Leaf(b)
-            leaves[row[1]] = b
-        else:
-            b = clusters[int(row[1])]
+        for i, row in enumerate(pairs):
+            if row[0] < len(words):
+                # if it is an original point read it from the centers array
+                a = words[int(row[0])]
+                a = _Leaf(a)
+                leaves[row[0]] = a
+            else:
+                # other wise read the cluster that has been created
+                a = clusters[int(row[0])]
 
-        # set a and b as children of the new node
-        distance = row[2]
-        cluster = Cluster(distance, children=[a, b])
+            if row[1] < len(words):
+                b = words[int(row[1])]
+                b = _Leaf(b)
+                leaves[row[1]] = b
+            else:
+                b = clusters[int(row[1])]
 
-        clusters[1 + i + len(pairs)] = cluster
-    return clusters
+            # set a and b as children of the new node
+            distance = row[2]
+            cluster = _Cluster(distance, children=[a, b])
 
+            clusters[1 + i + len(pairs)] = cluster
+        return clusters
 
-def cluster_words(words, max_cluster_size=5):
-    words = [nlp(word) for word in words]
-    # use the wordA.similarity(wordB) metric
-    # to get a similarity matrix
-    similarity_matrix = np.zeros((len(words), len(words)))
-    for i in range(len(words)):
-        for j in range(len(words)):
-            similarity_matrix[i, j] = words[i].similarity(words[j])
-    condensed_similarity_matrix = pdist(similarity_matrix)
-    # use the similarity matrix to cluster the words
-    Z = linkage(condensed_similarity_matrix, 'ward')
+    def cluster_words(self, words, max_cluster_size=5, max_distance=1.2):
+        words = [nlp(word) for word in words]
+        # use the wordA.similarity(wordB) metric
+        # to get a similarity matrix
+        similarity_matrix = np.zeros((len(words), len(words)))
+        for i in range(len(words)):
+            for j in range(len(words)):
+                similarity_matrix[i, j] = words[i].similarity(words[j])
+        condensed_similarity_matrix = pdist(similarity_matrix)
+        # use the similarity matrix to cluster the words
+        Z = linkage(condensed_similarity_matrix, 'ward')
 
-    clusters = convert_to_tree(Z, words)
+        clusters = self.convert_to_tree(Z, words)
 
-    # remove clusters that are too big
-    clusters = {k: v for k, v in clusters.items() if len(v.get_lemmas()) <= max_cluster_size}
+        # remove clusters that are too big and far apart
+        clusters = {k: v for k, v in clusters.items() if len(v.get_lemmas()) <= max_cluster_size}
+        clusters = {k: v for k, v in clusters.items() if v.distance <= max_distance}
+        return list(clusters.values())
 
-    return clusters.values()
+    def no_word_overlap(self, word, words):
+        # make sure the word does not equal,
+        # or is not a substring of any of the words, and vice versa
+        return (word not in words) and (not any([word in w for w in words]) and (not any([w in word for w in words])))
 
+    def get_clue(self, cluster, num_clues=3):
+        cluster_words = [lemma.text for lemma in cluster.get_lemmas()]
+        cluster_vectors = np.array([lemma.vector for lemma in cluster.get_lemmas()])
 
-def get_clue(cluster):
-    all_clues_less_cluster = [w for w in all_clues if w.text.lower() not in [c.text.lower() for c in cluster.get_lemmas()]]
+        # filter out clues that overlap with the words
+        possible_clues = []
+        possible_vectors = []
+        for clue, vector in zip(self.all_clue_words, self.all_clue_vectors):
+            if self.no_word_overlap(clue, cluster_words):
+                possible_clues.append(clue)
+                possible_vectors.append(vector)
+        possible_clues = np.array(possible_clues)
+        possible_vectors = np.array(possible_vectors)
 
-    # get the average vector for the cluster
-    cluster_avg = np.mean([w.vector for w in cluster.get_lemmas()], axis=0)
-    # get the distance from each word to the cluster average
-    distances = [np.linalg.norm(w.vector - cluster_avg) for w in all_clues_less_cluster]
-    # get the index of the word with the smallest distance
-    clue_index = np.argmin(distances)
-    # return the word with the smallest distance
-    clue = all_clues_less_cluster[clue_index]
-    return clue
+        # calculate the cosine distance between each clue and each word
+        cluster_dists = possible_vectors @ cluster_vectors.T
+        cluster_dists = 1 - cluster_dists / np.expand_dims(np.linalg.norm(cluster_vectors, axis=1),0) * np.expand_dims(np.linalg.norm(possible_vectors, axis=1), 1)
+        best_dists = np.sum(cluster_dists, axis=1) # - np.sum(your_dists, axis=1)
+        clue_idxs = np.argsort(best_dists)[:num_clues]
+        return list(possible_clues[clue_idxs])
 
+    def generate(self, words):
+        # cluster the words
+        clusters = self.cluster_words(words)
 
-def generate(grid):
+        # get the clue for each cluster
+        clusters_words = []
+        for cluster in clusters:
+            clusters_words.append([w.text for w in cluster.get_lemmas()])
+        
+        clues_words = []
+        for cluster in clusters:
+            clues_words.append(self.get_clue(cluster))
 
-    # get the words from the grid
-    words = []
-    for row in grid:
-        for cell in row:
-            revealed = cell['revealed']
-            # only consider unrevealed words
-            if not revealed:
-                words.append(cell['word'])
-    
-    # cluster the words
-    clusters = cluster_words(words)
-
-    # get the clue for each cluster
-    clusters_words = []
-    for cluster in clusters:
-        clusters_words.append([w.text for w in cluster.get_lemmas()])
-    
-    clues_words = []
-    for cluster in clusters:
-        clues_words.append(get_clue(cluster).text)
-
-    return clusters_words, clues_words
+        return clusters_words, clues_words
 
