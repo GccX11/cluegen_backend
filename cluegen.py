b9acfefbf0c2ca97b054b59b2c400fcbf6732a2c
GccX11 <GccX11@users.noreply.github.com>
2023-08-20 23:58:33 -0400

    Updated cluegen to no longer use spacy and use GloVE for much better clues now.

--- cluegen.py
+++ cluegen.py
@@ -1,19 +1,17 @@
 import os
-import spacy
+import pickle
+import tqdm
 import numpy as np
 from scipy.cluster.hierarchy import dendrogram, linkage
 from scipy.spatial.distance import pdist
 
 
-nlp = spacy.load('en_core_web_lg')
-
-
 class _Leaf(object):
     def __init__(self, word):
-        self.lemma = nlp(word)
+        self.lemma = word
     
     def __str__(self):
-        return self.lemma.text
+        return self.lemma
 
 
 class _Cluster(object):
@@ -38,10 +36,19 @@ class _Cluster(object):
 
 class ClueGenerator(object):
     def __init__(self, path):
+        with open(os.path.join(path, 'glove_dict.pkl'), 'rb') as f:
+            self.glove_dict = pickle.load(f)
+        self.glove_vectors = np.load(os.path.join(path, 'glove_vectors.npy'))
+
         # words originally from https://github.com/first20hours/google-10000-english
         # with additional filtering by me
         self.all_clue_words = np.load(os.path.join(path, 'all_clue_words.npy'))
         self.all_clue_vectors = np.load(os.path.join(path, 'all_clue_vectors.npy'))
+
+        print('ClueGenerator initialized')
+
+    def glove(self, word):
+        return self.glove_vectors[self.glove_dict[word]]
 
     # create a proper tree structure,
     # where each node has the average distance of its children
@@ -73,14 +80,19 @@ class ClueGenerator(object):
             clusters[1 + i + len(pairs)] = cluster
         return clusters
 
-    def cluster_words(self, words, max_cluster_size=5, max_distance=1.2):
-        words = [nlp(word) for word in words]
+    def cosine_similarity(self, x, y):
+        return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
+
+    def cluster_words(self, words, max_cluster_size=5):
         # use the wordA.similarity(wordB) metric
         # to get a similarity matrix
         similarity_matrix = np.zeros((len(words), len(words)))
+        # TODO: vectorize this
         for i in range(len(words)):
+            vector_i = self.glove(words[i].lower())
             for j in range(len(words)):
-                similarity_matrix[i, j] = words[i].similarity(words[j])
+                vector_j = self.glove(words[j].lower())
+                similarity_matrix[i, j] = self.cosine_similarity(vector_i, vector_j)
         condensed_similarity_matrix = pdist(similarity_matrix)
         # use the similarity matrix to cluster the words
         Z = linkage(condensed_similarity_matrix, 'ward')
@@ -89,7 +101,7 @@ class ClueGenerator(object):
 
         # remove clusters that are too big and far apart
         clusters = {k: v for k, v in clusters.items() if len(v.get_lemmas()) <= max_cluster_size}
-        clusters = {k: v for k, v in clusters.items() if v.distance <= max_distance}
+        #clusters = {k: v for k, v in clusters.items() if v.distance <= max_distance}
         return list(clusters.values())
 
     def no_word_overlap(self, word, words):
@@ -97,39 +109,39 @@ class ClueGenerator(object):
         # or is not a substring of any of the words, and vice versa
         return (word not in words) and (not any([word in w for w in words]) and (not any([w in word for w in words])))
 
-    def get_clue(self, cluster, num_clues=3):
-        cluster_words = [lemma.text for lemma in cluster.get_lemmas()]
-        cluster_vectors = np.array([lemma.vector for lemma in cluster.get_lemmas()])
+    def get_clue(self, cluster_words, other_words, num_clues=3):
+        cluster_words = set(w.lower() for w in cluster_words)
+        other_words = set(w.lower() for w in other_words)
 
-        # filter out clues that overlap with the words
-        possible_clues = []
-        possible_vectors = []
-        for clue, vector in zip(self.all_clue_words, self.all_clue_vectors):
-            if self.no_word_overlap(clue, cluster_words):
-                possible_clues.append(clue)
-                possible_vectors.append(vector)
-        possible_clues = np.array(possible_clues)
-        possible_vectors = np.array(possible_vectors)
+        vectors = np.array([self.glove(word.lower()) for word in cluster_words])
 
-        # calculate the cosine distance between each clue and each word
-        cluster_dists = possible_vectors @ cluster_vectors.T
-        cluster_dists = 1 - cluster_dists / np.expand_dims(np.linalg.norm(cluster_vectors, axis=1),0) * np.expand_dims(np.linalg.norm(possible_vectors, axis=1), 1)
-        best_dists = np.sum(cluster_dists, axis=1) # - np.sum(your_dists, axis=1)
-        clue_idxs = np.argsort(best_dists)[:num_clues]
-        return list(possible_clues[clue_idxs])
+        dists = self.all_clue_vectors @ vectors.T
+        dists = 1 - dists / (np.expand_dims(np.linalg.norm(vectors, axis=1),0) * np.expand_dims(np.linalg.norm(self.all_clue_vectors, axis=1), 1))
+        best_dists = np.sum(dists, axis=1) # - np.sum(your_dists, axis=1)
+        clue_idxs = np.argsort(best_dists)[:num_clues+len(cluster_words)+len(other_words)]
+        clues = list(self.all_clue_words[clue_idxs])
+        # filter out words that are already in the cluster
+        clues = [clue for clue in clues if self.no_word_overlap(clue, cluster_words) and self.no_word_overlap(clue, other_words)]
+        return clues[:num_clues]
 
     def generate(self, words):
+        print('got', len(words), 'words')
         # cluster the words
         clusters = self.cluster_words(words)
-
-        # get the clue for each cluster
         clusters_words = []
-        for cluster in clusters:
-            clusters_words.append([w.text for w in cluster.get_lemmas()])
-        
+        print('getting clusters')
+        for cluster in tqdm.tqdm(clusters):
+            cluster_words = cluster.get_lemmas()
+            clusters_words.append(cluster_words)
+        print('got', len(clusters_words), 'clusters')
+
+        # get clues for each cluster
         clues_words = []
-        for cluster in clusters:
-            clues_words.append(self.get_clue(cluster))
+        print('getting clues')
+        for cluster_words in tqdm.tqdm(clusters_words):
+            other_words = [w for w in words if w not in cluster_words]
+            clues_words.append(self.get_clue(cluster_words, other_words))
+        print('got', len(clues_words), 'clues')
 
         return clusters_words, clues_words
 
